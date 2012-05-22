6befb1384ef9bb3cce067ae53fc39e1c41785581
Julien Maupetit <julien@maupetit.net>
2012-05-22 10:27:00 +0200

    Fixed django-oscar 0.2 checkout compatibility

--- paypal/express/urls.py
+++ paypal/express/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
         name='paypal-success-response'),
     url(r'^cancel/', views.CancelResponseView.as_view(),
         name='paypal-cancel-response'),
-    url(r'^place-order/', views.SuccessResponseView.as_view(),
+    url(r'^place-order/', views.SuccessResponseView.as_view(preview=True),
         name='paypal-place-order'),
     # Callback for getting shipping options for a specific basket
     url(r'^shipping-options/(?P<basket_id>\d+)/', views.ShippingOptionsView.as_view(),
